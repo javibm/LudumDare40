a361f3a8dd8443a8079ee74535702c7e68c16f05
MarcosM10 <marcos.diefer1@gmail.com>
2017-12-02 15:50:52 +0100

    + GameMetaManager first iteration

---
+++ Assets/Scripts/Managers/GameMetaManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMetaManager : Singleton<GameMetaManager> 
+{
+	public static OfficeManager Office
+	{
+		get
+		{
+			return Instance.office;
+		}
+	}
+
+	public static MoneyManager Money
+	{
+		get
+		{
+			return Instance.money;
+		}
+	}
+
+	protected new void Awake()
+	{
+		office = new OfficeManager(officeInitialSize);
+		money = new MoneyManager(initialMoney);
+	}
+
+	private OfficeManager office;
+	private MoneyManager money;
+
+	[SerializeField]
+	private int officeInitialSize = 2;
+	[SerializeField]
+	private int initialMoney = 1000;
+}
--- Assets/Scripts/OfficeGenerator.cs
+++ Assets/Scripts/OfficeGenerator.cs
@@ -5,7 +5,6 @@ using UnityEngine.AI;
 
 public class OfficeGenerator : MonoBehaviour 
 {
-
 	public List<Transform> DeskTransformList
 	{
 		get
