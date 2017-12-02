09a3ae41ba6961d660a695539874ad5351a842f7
CarlotaEsca <carlota.esca91@gmail.com>
2017-12-02 16:24:36 +0100

    + EmployeeController created.

---
+++ Assets/Scripts/Employee/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmployeeController : MonoBehaviour {
+
+	public float MoneyGenerated
+	{
+		get;
+		set;
+	}
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		employeeStateController = GetComponent<EmployeeStateController>();
+		employeeMovementController = GetComponent<EmployeeMovementController>();
+	}
+
+	public EmployeeController(float moneyGenerated)
+	{
+		MoneyGenerated = moneyGenerated;
+	}
+
+	private EmployeeStateController employeeStateController;
+	private EmployeeMovementController employeeMovementController;
+}
