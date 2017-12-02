63027cd1b6e715db8b8e8b20407ebb217e90fe3b
Javi <javibm6@gmail.com>
2017-12-02 19:04:42 +0100

    + Added simple animation controller

---
+++ Assets/Scripts/Employee/EmployeeAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EmployeeAnimationController : MonoBehaviour
+{
+
+  void Awake()
+  {
+    animatorController.GetComponent<Animator>();
+  }
+
+  public void WalkAnim()
+  {
+    animatorController.CrossFade("Walking", 0.0f);
+  }
+
+  public void IdleAnim()
+  {
+    animatorController.CrossFade("Idle", 0.0f);
+  }
+
+  public void WorkAnim()
+  {
+    animatorController.CrossFade("Work", 0.0f);
+  }
+
+  private Animator animatorController;	
+}
