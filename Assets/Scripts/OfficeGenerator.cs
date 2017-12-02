b3c59eb86118932ebd8792235d5c6ac35fd96092
MarcosM10 <marcos.diefer1@gmail.com>
2017-12-02 13:29:32 +0100

    + Office Generation adjustments

--- Assets/Scripts/OfficeGenerator.cs
+++ Assets/Scripts/OfficeGenerator.cs
@@ -58,12 +58,12 @@ public class OfficeGenerator : MonoBehaviour
 		go = Instantiate(wallPrefab);
 		go.transform.SetParent(officeRootTransform);
 		go.transform.localPosition = new Vector3(cellSize * (step-1), 0f, 0f);
-		go.transform.eulerAngles = new Vector3(0f, 90, 0f);
+		go.transform.eulerAngles = new Vector3(0f, -90, 0f);
 
 		go = Instantiate(wallPrefab);
 		go.transform.SetParent(officeRootTransform);
 		go.transform.localPosition = new Vector3(0f, 0f, cellSize * (step-1));
-		go.transform.eulerAngles = new Vector3(0f, 180, 0f);
+		go.transform.eulerAngles = new Vector3(0f, 0, 0f);
 
 		// Desk generation
 		for(int i = 0; i < step; ++i)
