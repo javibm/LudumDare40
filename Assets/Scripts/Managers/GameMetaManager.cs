33a479d23469fcc121ee846ff4fcff6646357829
Javi <javibm6@gmail.com>
2017-12-03 20:31:55 +0100

    + Fixed bug on GameMetaManager

--- Assets/Scripts/Managers/GameMetaManager.cs
+++ Assets/Scripts/Managers/GameMetaManager.cs
@@ -27,19 +27,7 @@ public class GameMetaManager : Singleton<GameMetaManager>
 			return Instance.gameStats.MaxDaysWithNegativeMoney;
 		}
 	}
-
-	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			GameMetaManager.Office.TryExpand();
-		}
-		if(Input.GetKeyDown(KeyCode.M))
-		{
-			GameMetaManager.Employee.TryCreateNewEmployee(0,1);
-		}
-	}
-
+	
 	public static System.Action OnLoseGame;
 
 	protected new void Awake()
@@ -73,6 +61,14 @@ public class GameMetaManager : Singleton<GameMetaManager>
 		if(Input.GetKey(KeyCode.M))
 		{
 			money.AddMoney(10);
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GameMetaManager.Office.TryExpand();
+		}
+		if(Input.GetKeyDown(KeyCode.L))
+		{
+			GameMetaManager.Employee.TryCreateNewEmployee(0,1);
 		}
 	}
 
